b0f51bdde4d5a059805440e309c479f90c248992
Deakcor <40196601+deakcor@users.noreply.github.com>
2020-06-24 15:44:06 +0200

    open file

--- src/app/components/top-menu/template-popover.component.ts
+++ src/app/components/top-menu/template-popover.component.ts
@@ -1,20 +1,35 @@
 import { Component, Input } from "@angular/core";
-import { NavParams, PopoverController } from '@ionic/angular';
+import {  PopoverController } from '@ionic/angular';
 
 @Component({
   templateUrl: './template-popover.component.html',
   })
   export class PopoverPage {
-    constructor(public navParams:NavParams,public popoverCtrl: PopoverController) {
-        this.id = this.navParams.get('id');
+
+    @Input() id:number=10
+
+    constructor(public popoverCtrl: PopoverController) {
     }
   
     close() {
       
     }
-    id:number=0
+    
 
     testClick(){
+      this.popoverCtrl.dismiss()
+    }
+
+    load(files){
+      console.log(files)
+      //var myBlob = new Blob([new Uint8Array(file)], {type: "octet/stream"});
+
+      
+      this.popoverCtrl.dismiss()
+
+    }
+
+    save(){
       this.popoverCtrl.dismiss()
     }
   }
