10a977dbfc91af403ec8f20357c955b8e67647de
Deakcor <deak24cor@gmail.com>
2020-07-09 22:30:45 +0200

    need to finish csv to structure

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -130,6 +130,103 @@ export class GlobalService {
 
   }
 
+  CSVToArray( strData, strDelimiter = ',' ): string[][]{
+
+    // Create a regular expression to parse the CSV values.
+    const objPattern = new RegExp(
+        (
+            // Delimiters.
+            '(\\' + strDelimiter + '|\\r?\\n|\\r|^)' +
+
+            // Quoted fields.
+            '(?:"([^"]*(?:""[^"]*)*)"|' +
+
+            // Standard fields.
+            '([^"\\' + strDelimiter + '\\r\\n]*))'
+        ),
+        'gi'
+        );
+
+
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    const arrData: string[][] = [[]];
+
+    // Create an array to hold our individual pattern
+    // matching groups.
+    let arrMatches = null;
+
+
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec( strData )){
+
+        // Get the delimiter that was found.
+        const strMatchedDelimiter = arrMatches[ 1 ];
+
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (
+            strMatchedDelimiter.length &&
+            strMatchedDelimiter !== strDelimiter
+            ){
+
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push( [] );
+
+        }
+
+        let strMatchedValue;
+
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[ 2 ]){
+
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            strMatchedValue = arrMatches[ 2 ].replace(
+                new RegExp( '""', 'g' ),
+                '"'
+                );
+
+        } else {
+
+            // We found a non-quoted value.
+            strMatchedValue = arrMatches[ 3 ];
+
+        }
+
+
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[ arrData.length - 1 ].push( strMatchedValue );
+    }
+
+    // Return the parsed data.
+    return( arrData );
+}
+
+  async importCsvFile(file: File, separator = '.') {
+    const  csvArray: string[][] = this.CSVToArray(await file.text());
+
+    if (csvArray.length > 1){
+      let languages: string[] = [];
+      for (let k = 1; k < csvArray[0].length; k++){
+        languages.push(csvArray[0][k]);
+      }
+      if (languages.length > 0){
+        for (let k = 1; k < csvArray.length; k++){
+          // csvArray[k]
+        }
+      }
+    }
+
+  }
+
   async importJsonFiles(files: object[], languages: string[]) {
     this.languages = languages.filter((e, i) => languages.indexOf(e) === i);
     const newStructure = new Folder('root', undefined);
